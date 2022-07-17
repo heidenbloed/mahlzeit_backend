f715ab5ea7e0a7580c992758e760c7524f088513
heidenbloed <git@heidenblut.eu>
2022-07-17 23:02:29 +0200

    Update docker restart policies. + Improve init script. + Fix nginx configuration. + Fix django debug setting.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', False)
+DEBUG = os.environ.get('DJANGO_DEBUG', "false").lower() in ["true", "yes", "1"]
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 env_allowed_host = os.environ.get('DJANGO_ALLOWED_HOST')
--- server_init.sh
+++ server_init.sh
@@ -1,14 +1,25 @@
-echo "enter the sudo password, please"
-read PW
+#!/bin/sh
+
+stty -echo
+printf "Enter the sudo password, please:"
+read -s PW
+stty echo
+printf "\n"
 docker-compose -f docker-compose.yml -f docker-compose.production.yml down
-docker-compose -f docker-compose.yml -f docker-compose.production.yml build --build-arg UID=$(id -u)
-echo $PW | sudo chown -R $USER:www-data /var/www/recipe_db_backend/static_volume/
-echo $PW | sudo chown -R $USER:www-data /var/www/recipe_db_backend/media_volume/
-echo $PW | sudo chmod -R g-w /var/www/recipe_db_backend/static_volume/
-echo $PW | sudo chmod -R g-w /var/www/recipe_db_backend/media_volume/
-docker-compose -f docker-compose.yml -f docker-compose.production.yml exec web python manage.py collectstatic --no-input --clear
-docker-compose -f docker-compose.yml -f docker-compose.production.yml exec web python manage.py migrate --noinput
-docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
-echo $PW | sudo cp recipe_db_backend.nginx.conf /etc/nginx/sites-available/
-echo $PW | sudo ln -s /etc/nginx/sites-available/recipe_db_backend.nginx.conf /etc/nginx/sites-enabled/
-echo $PW | sudo systemctl restart nginx
+docker-compose -f docker-compose.yml -f docker-compose.production.yml build
+if [ $? -eq 0 ]; then
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
+  echo $PW | sudo chown -R $USER:www-data "/var/www/recipe_db_backend/static_volume/"
+  echo $PW | sudo chown -R $USER:www-data "/var/www/recipe_db_backend/media_volume/"
+  echo $PW | sudo chmod -R g-w "/var/www/recipe_db_backend/static_volume/"
+  echo $PW | sudo chmod -R g-w "/var/www/recipe_db_backend/media_volume/"
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml exec web python manage.py collectstatic --no-input --clear
+  docker-compose -f docker-compose.yml -f docker-compose.production.yml exec web python manage.py migrate --noinput
+  echo $PW | sudo cp recipe_db_backend.nginx.conf /etc/nginx/sites-available/
+  if [ ! -f "/etc/nginx/sites-enabled/recipe_db_backend.nginx.conf" ]; then
+    echo $PW | sudo ln -s "/etc/nginx/sites-available/recipe_db_backend.nginx.conf" "/etc/nginx/sites-enabled/"
+  fi
+  echo $PW | sudo systemctl restart nginx
+else
+  echo "Docker build failed."
+fi
