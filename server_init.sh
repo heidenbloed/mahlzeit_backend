877912154e85035880bf269550cc1740d3f9e0d1
heidenbloed <69988488+heidenbloed@users.noreply.github.com>
2025-07-03 17:42:20 +0200

    Update owner of backup folder.

--- server_init.sh
+++ server_init.sh
@@ -13,6 +13,7 @@ if [ $? -eq 0 ]; then
   echo $PW | sudo chown -R $USER:www-data "/var/www/recipe_db_backend/media_volume/"
   echo $PW | sudo chmod -R g-w "/var/www/recipe_db_backend/static_volume/"
   echo $PW | sudo chmod -R g-w "/var/www/recipe_db_backend/media_volume/"
+  echo $PW | sudo chown -R $USER:$USER "$HOME/recipe_db_backup_data/"
   docker-compose -f docker-compose.yml -f docker-compose.production.yml exec web python manage.py collectstatic --no-input --clear
   docker-compose -f docker-compose.yml -f docker-compose.production.yml exec web python manage.py migrate --noinput
   echo $PW | sudo cp recipe_db_backend.nginx.conf /etc/nginx/sites-available/
